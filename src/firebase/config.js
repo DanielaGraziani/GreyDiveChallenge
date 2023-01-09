e2cf16579a60e19734d604eaac7ef160c5551275
Daniela Diaz Graziani <danieladiazgraziani12@gmail.com>
2023-01-09 14:07:00 -0300

    Add firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxp1JMJAmatGaFgh_YT362WYhL8_NUHdw",
+  authDomain: "greydive-d5e0c.firebaseapp.com",
+  projectId: "greydive-d5e0c",
+  storageBucket: "greydive-d5e0c.appspot.com",
+  messagingSenderId: "951116348993",
+  appId: "1:951116348993:web:5422447c2535116450d0c0",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
