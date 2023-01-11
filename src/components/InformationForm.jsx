94797face0c527f589d19987b13a55c8faf9fbcc
Daniela Diaz Graziani <danieladiazgraziani12@gmail.com>
2023-01-10 22:27:12 -0300

    Add loading

--- src/components/InformationForm.jsx
+++ src/components/InformationForm.jsx
@@ -7,6 +7,8 @@ const InformationForm = () => {
   const db = getFirestore(app);
 
   const [getInfo, setGetInfo] = useState([]);
+  const [loading, setLoading] = useState(true); //add new state variable
+
 
   useEffect(() => {
     const getUserInfo = async () => {
@@ -18,6 +20,7 @@ const InformationForm = () => {
           docs.push({ ...doc.data(), id: doc.id });
         });
         setGetInfo(docs);
+        setLoading(false);
         console.log(docs);
       } catch (error) {
         console.log(error);
@@ -30,15 +33,15 @@ const InformationForm = () => {
   return (
     
     <div>
-      <h2 className="text-gray-900 font-medium text-2xl mt-12 mb-10">Users Information</h2> 
-      <div className="grid grid-cols-1 gap-16 sm:grid-cols-2 lg:grid-cols-3 px-36">
+      <h2 className="text-gray-900 font-medium text-2xl mt-10 mb-10">Users Information</h2> 
+      {loading && <div className="loading">Loading...</div>}
+      <div className="grid grid-cols-1 gap-16 mb-12 sm:grid-cols-2 lg:grid-cols-3 px-36">
           {getInfo.map((list) => (
             <div key={list.id}>
               <InformationGrid id={list.id} name={list.full_name} email={list.email} country={list.country_of_origin} birth_date={new Date(list.birth_date).toLocaleDateString()}/>
             </div>
           ))}
       </div>
-       
     </div>
   );
 };
